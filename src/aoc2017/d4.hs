1613d22f2539ff5eb679e6dd97aae5f415455cf3
Semyon <kontaktzielinski@protonmail.com>
2021-03-19 22:08:08 +0100

    p2

--- src/aoc2017/d4.hs
+++ src/aoc2017/d4.hs
@@ -3,6 +3,6 @@ import Data.List
 main :: IO ()
 main = do
   xs <- map words . lines <$> readFile "../data17/data04.txt" 
-  print $ length $ filter (\x -> x == (nub x)) xs
+  print $ length $ filter (\x -> x == nub x) $ map (map sort) xs
 
 
